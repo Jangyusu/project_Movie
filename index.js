9095ece2192e0dc53c139603d8aad6a043752488
yusu <dbtnss@naver.com>
2020-03-09 16:41:55 +0900

    movie

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 var menuButton = document.getElementsByClassName("menu_button")[0],
-    menu = document.getElementsByClassName("menu")[0];
-
-console.log(menuButton);
-console.log(menu);
+    menu = document.getElementsByClassName("menu")[0],
+    start = document.getElementsByClassName("start")[0],
+    header = document.getElementsByTagName("header")[0],
+    director = document.getElementsByClassName("director")[0],
+    scroll = document.getElementsByClassName("scroll")[0],
+    section = document.getElementsByTagName("section");
 
 menuButton.addEventListener("click", function() {
     event.preventDefault();
 
     menuButton.classList.toggle("active");
     menu.classList.toggle("active");
-});
+}); // 메뉴 토글
+
+start.addEventListener("click", function() {
+    event.preventDefault();
+
+    start.classList.add("active");
+
+    setTimeout(function() {
+        header.classList.add("active");
+        director.classList.add("active");
+        director.children[0].children[0].classList.add("active");
+        director.children[2].children[0].classList.add("active");
+        scroll.classList.add("active");
+        scroll.children[1].classList.add("active");
+        section[0].classList.add("active");
+        section[0].children[2].classList.add("active");
+    }, 3000);
+}); // 시작버튼 생성
+
+for (var i = 0; i < menu.children.length; i++) {
+    menu.children[i].children[0].addEventListener("click", function() {
+        var orgTarget = this.getAttribute("href"),
+            tabTarget = orgTarget.replace("#", "");
+
+        event.preventDefault();
+
+        for (var i = 0; i < section.length; i++) {
+            section[i].classList.remove("click");
+        }
+
+        section[tabTarget].classList.add("click");
+    });
+}
