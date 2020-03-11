6f46d0944dd97727954a5ca49e31ec401d69df12
yusu <dbtnss@naver.com>
2020-03-11 16:58:40 +0900

    cating trailer

--- js/index.js
+++ js/index.js
@@ -7,7 +7,9 @@ var menuButton = document.querySelector(".menu_button"),
     section = document.querySelectorAll("section"),
     footer = document.querySelector("footer"),
     indicator = document.querySelectorAll(".indicator"),
-    Index = 0;
+    pageIndex = 0,
+    synopsisIndex = 0,
+    castingIndex = 0;
 
 function getIndex(target) {
     var i = 0;
@@ -46,14 +48,15 @@ menuButton.addEventListener("click", function() {
 
 for (var i = 0; i < menu.children.length; i++) {
     menu.children[i].addEventListener("click", function() {
-        var orgTarget = this.querySelector("a").getAttribute("href");
-        tabTarget = orgTarget.replace("#", "");
+        var pressedIndex = getIndex(event.target);
+
         menuButton.classList.toggle("active");
         menu.classList.toggle("active");
-        for (var i = 0; i < section.length; i++) {
-            section[i].classList.remove("active");
-        }
-        section[tabTarget].classList.add("active");
+
+        section[pageIndex].classList.remove("active");
+        section[pressedIndex].classList.add("active");
+
+        pageIndex = pressedIndex;
     });
 } // 메뉴 선택
 
@@ -61,33 +64,34 @@ for (var i = 0; i < indicator[0].children.length; i++) {
     indicator[0].children[i].addEventListener("click", function() {
         var pressedIndex = getIndex(event.target);
 
-        indicator[0].children[Index].classList.remove("active");
+        indicator[0].children[synopsisIndex].classList.remove("active");
         indicator[0].children[pressedIndex].classList.add("active");
+
+        section[1].querySelectorAll("article")[synopsisIndex].classList.remove("active");
+        section[1].querySelectorAll("article")[pressedIndex].classList.add("active");
 
         section[1].style = "background-image: url('img/Synopsis_0" + pressedIndex + ".jpg')";
 
-        section[1].querySelectorAll("article")[Index].classList.remove("active");
-        section[1].querySelectorAll("article")[pressedIndex].classList.add("active");
-
-        Index = pressedIndex;
+        synopsisIndex = pressedIndex;
     });
 } // Synopsis 페이지 구현
 
 section[2].querySelector("a").addEventListener("click", function() {
     event.preventDefault();
 
-    this.classList.toggle("active");
-    indicator[1].querySelector("div").classList.toggle("active");
+    this.classList.toggle("active"); // 화살표
+    indicator[1].querySelector("div").classList.toggle("active"); // 배우 리스트
 });
 
-for (var i = 0; i < indicator[1].querySelectorAll("div img").length; i++) {
-    indicator[1].querySelectorAll("div img")[i].addEventListener("click", function() {
-        event.preventDefault();
-        var pressedIndex2 = getIndex(event.target);
+for (var i = 0; i < indicator[1].querySelectorAll("div div img").length; i++) {
+    indicator[1].querySelectorAll("div div img")[i].addEventListener("click", function() {
+        var pressedIndex = getIndex(event.target);
 
-        console.log(event.target);
+        section[2].querySelectorAll("article")[castingIndex].classList.remove("active");
+        section[2].querySelectorAll("article")[pressedIndex].classList.add("active");
 
-        indicator[1].querySelector("div").classList.remove("active");
-        section[2].style = "background-image: url('img/Casting_0" + pressedIndex2 + ".jpg')";
+        section[2].style = "background-image: url('img/Casting_0" + pressedIndex + ".jpg')";
+
+        castingIndex = pressedIndex;
     });
 } // Casting 페이지 구현
